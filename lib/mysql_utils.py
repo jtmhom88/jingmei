aa06b57756ecd362a876b41f26b9f939d37c8910
Johnny Hom <jhom@kochava.com>
2015-12-27 00:45:02 -0800

    adding function get_article_idx

--- lib/mysql_utils.py
+++ lib/mysql_utils.py
@@ -13,7 +13,9 @@ import mysql.connector
 #      'host': 'xxx'
 # }
 
-# Pass it a config object, and it returns a connection object
+
+# Pass it a config object
+# Returns a connection object
 def connect(config):
   # Connecting to Mysql
   try:
@@ -28,7 +30,7 @@ def connect(config):
 
 
 # Pass it connection, url, and tag, and it inserts it
-# tag example 'wsj'
+# datasource example 'wsj'
 def insert_url(connection, url, datasource):
   cursor = connection.cursor(buffered=True) 
   query = "INSERT IGNORE INTO sentiment.article_urls (url,datasource) " \
@@ -42,6 +44,8 @@ def insert_url(connection, url, datasource):
     print('last insert id not found, nothing inserted')
   pass
 
+# Pass it connection, url, datasource, article_idx, article_line_no, article_text 
+# Function inserts line into database
 def insert_article_line(connection, url, datasource, article_idx, article_line_no, article_text ):
   cursor = connection.cursor(buffered=True) 
   query = "select count(*) from sentiment.article_lines where article_url = %s AND article_line_no = %s " 
@@ -54,8 +58,8 @@ def insert_article_line(connection, url, datasource, article_idx, article_line_n
   except Exception, e:
     print "Exception: cannot execute query %s" % (query)
     raise e
-
   pass
+
   if exists > 0:
     print "Failed: trying to re-insert row"
   else:
@@ -80,13 +84,35 @@ def insert_article_line(connection, url, datasource, article_idx, article_line_n
     finally:
       pass
 
+# Function to takes connection, url
+# Returns article_idx
+# If none found, it returns 0
+def get_article_idx(connection, url):
+  cursor = connection.cursor(buffered=True) 
+  query = "select idx from sentiment.article_urls where url = %s " 
+  args = ((url,))
+  try:
+    cursor.execute(query, args)
+    row = cursor.fetchone()
+    article_idx = row[0]
+    pass
+  except Exception, e:
+    print "Exception: none returned when executing query %s" % (query)
+    return 0
+    #raise e
+  else:
+    return article_idx
+  pass
 
+##Testing block
 # if __name__ == '__main__':
-#     connection = connect(config)
-#     url='http://www.msn.com/en-us/money/taxes/trump-tax-plan-would-cut-federal-revenue-22percent/ar-BBnPh7Q'
-#     datasource='msn'
-#     article_idx=58
-#     article_line_no=0
-#     article_text='Donald Duck'
-#     insert_article_line(connection, url, datasource, article_idx, article_line_no, article_text )
-#     pass
+#   connection = connect(config)
+#   url='http://www.msn.com/en-us/money/taxes/trump-tax-plan-would-cut-federal-revenue-22percent/ar-BBnPh7Q'
+#   url='foobar'
+#   datasource='msn'
+#   article_idx=58
+#   article_line_no=0
+#   article_text='Donald Duck'
+#   #insert_article_line(connection, url, datasource, article_idx, article_line_no, article_text )
+#   print get_article_idx(connection,url)
+#   pass
