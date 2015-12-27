7226c2eb498ad6ca1d1befdf62fef3f8776b508f
Johnny Hom <jhom@kochava.com>
2015-12-27 00:07:57 -0800

    Adding insert_article_line

--- lib/mysql_utils.py
+++ lib/mysql_utils.py
@@ -6,13 +6,12 @@ import mysql.connector
 
 #########################
 # for mysql connection
-"""
-config = { 
-    'user': mysql_user, 
-    'password': mysql_pwd, 
-    'host': mysql_ip
-}
-"""
+
+# config = { 
+#     'user': 'xxx', 
+#     'password': 'xxx', 
+#     'host': 'xxx'
+# }
 
 # Pass it a config object, and it returns a connection object
 def connect(config):
@@ -30,18 +29,64 @@ def connect(config):
 
 # Pass it connection, url, and tag, and it inserts it
 # tag example 'wsj'
-def insert_url(connection, url, tag):
+def insert_url(connection, url, datasource):
   cursor = connection.cursor(buffered=True) 
   query = "INSERT IGNORE INTO sentiment.article_urls (url,datasource) " \
             "VALUES(%s,%s)"
   #args = ('http://www.wsj.com/articles/the-best-coach-approach-promote-or-poach-1449792464', 'wsj')
-  args = (url, tag)
+  args = (url, datasource)
   cursor.execute(query, args)
   if cursor.lastrowid:
     print('last insert id', cursor.lastrowid)
   else:
-    print('last insert id not found')
+    print('last insert id not found, nothing inserted')
   pass
 
-#if __name__ == '__main__':
-#    connect()
+def insert_article_line(connection, url, datasource, article_idx, article_line_no, article_text ):
+  cursor = connection.cursor(buffered=True) 
+  query = "select count(*) from sentiment.article_lines where article_url = %s " 
+  args = ((url,))
+  try:
+    cursor.execute(query, args)
+    row = cursor.fetchone()
+    exists = row[0]
+    pass
+  except Exception, e:
+    print "Exception: cannot execute query %s" % (query)
+    raise e
+
+  pass
+  if exists > 0:
+    print "Failed: trying to re-insert row"
+  else:
+    print "inserting"
+    try:
+      query = "INSERT IGNORE INTO sentiment.article_lines (article_url, datasource, article_idx, article_line_no, article_text) " \
+            "VALUES(%s,%s,%s,%s,%s)"
+      args = (url,datasource, article_idx,article_line_no,article_text)
+      print args
+      cursor.execute(query, args)
+      if cursor.lastrowid:
+        print('last insert id', cursor.lastrowid)
+      else:
+        print('last insert id not found, nothing inserted')
+      pass
+    except Exception, e:
+      msg = "Exception: Failed to insert query %s" % (query)
+      print msg
+      raise e
+    else:
+      pass
+    finally:
+      pass
+
+
+# if __name__ == '__main__':
+#     connection = connect(config)
+#     url='http://www.msn.com/en-us/money/taxes/trump-tax-plan-would-cut-federal-revenue-22percent/ar-BBnPh7Q'
+#     datasource='msn'
+#     article_idx=58
+#     article_line_no=0
+#     article_text='Donald Duck'
+#     insert_article_line(connection, url, datasource, article_idx, article_line_no, article_text )
+#     pass
