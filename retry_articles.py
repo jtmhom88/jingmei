bd9efff879a58432f0bee48e91d8a580f4e57e2e
jtmhom88 <johnnyhom@gmail.com>
2016-07-16 15:18:46 -0700

    Fixing collector Taking care of ips

--- retry_articles.py
+++ retry_articles.py
@@ -20,20 +20,55 @@ from mysql.connector.errors import Error
 try:
 	mysql_user = os.environ['MYSQL_USER']
 	mysql_pwd = os.environ['MYSQL_PWD']
-	mysql_ip = os.environ['MYSQL_IP']
+	#mysql_ip = os.environ['MYSQL_IP']
+	mysql_ip = os.environ['MYSQL_IPLOCAL']
 	myconf = {"user": mysql_user,
 	    "password":mysql_pwd,
 	    "host":mysql_ip}
 	pass
 except Exception, e:
 	myconf = { 
-	"user":"bexxx",
-	"password":"misxxx",
+	#"user":"root",
+	"user":"beautiful",
+	"password":"miso314",
 	"host":"127.0.0.1"
+	#"host":"54.244.239.236"
 	}
 	print "Cannot find env varibles",e
 	print "Using local configs"
 	pass
+
+
+if __name__ == '__main__':
+	print myconf
+	conn = mysql.connector.connect(**myconf) # Choose connection config set
+	print "path:" + lib_path+'/cookies.txt'
+	wsjopener = getcookieopener(lib_path+'/cookies.txt')
+	cookieopeners = {"wsj" : wsjopener}
+
+	#article_list = getarticlelist(datasources)
+	#for k in article_list.iterkeys():
+	#	print k.encode('utf-8'),article_list[k]
+	#	insert_url(conn, k, article_list[k][0], article_list[k][1])
+
+	#Get un-downloaded articles
+	#relevant_article_list = get_urls_with_codes(conn, 0)
+	relevant_article_list = []
+
+	#Add articles with error "IncompleteRead"
+	relevant_article_list.extend(get_urls_with_codes(conn, 13))
+
+	#Add articles with error "timeout"
+	relevant_article_list.extend(get_urls_with_codes(conn, 10))
+
+	#Add articles with other socket error
+	relevant_article_list.extend(get_urls_with_codes(conn, 15))
+
+	#Add articles with URLError
+	relevant_article_list.extend(get_urls_with_codes(conn, 11))
+
+	#Add articles with HTTPError
+	relevant_article_list.extend(get_urls_with_codes(conn, 12))
 
 	#Get un-downloaded articles
 	relevant_article_list = get_urls_with_codes(conn, 0)
